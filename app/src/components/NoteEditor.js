dcffc8fb1a2ab3d83f56a54f0dba839c64b5280b
zhanglun <zhanglun1410@gmail.com>
2016-12-15 20:31:57 +0800

    update editor

--- app/src/components/NoteEditor.js
+++ app/src/components/NoteEditor.js
@@ -1,84 +1,84 @@
 import React, { Component } from 'react';
+import { Editor, ContentState, EditorState } from 'draft-js';
+console.log(ContentState);
+import 'medium-draft/lib/index.css';
 import * as Note from '../helper/db/note';
 
 
 export default class EditorComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: {
-        title: '',
-        content: '',
-      },
+    constructor(props) {
+        super(props);
+        this.state = {
+            note: {
+                title: '',
+                content: '',
+            },
+            editorState: EditorState.createEmpty(),
+        };
+        this.onChange = (editorState) => {
+            this.setState({ editorState });
+        };
+
+    }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+        this.refs.editor.focus();
+    }
+
+    componentWillReceiveProps(nextprops) {
+        let { note } = nextprops;
+        let NoteRef = firebase.database().ref('user-notes/' + currentUser.uid + '/' + note.key);
+        NoteRef.on('value', (snapshot) => {
+            console.log(snapshot.val());
+            let note = Object.assign({}, snapshot.val(), { key: snapshot.ref.key });
+            this.setState({ note, });
+            this.setState({
+                editorState: createEditorState(note.content)
+            });
+        });
     };
-    this.timer = null;
-    this.editor = null;
-  }
 
-  componentWillMount() {
-  }
+    handleTitleChange(event) {
+        let { note } = this.state;
+        note.title = event.target.value;
+        this.setState({ note, });
+    }
 
-  componentDidMount() {
-    this.editor = this.refs.noteTextarea;
-  }
-
-  componentWillReceiveProps(nextprops) {
-    let { note } = nextprops;
-    let editor = this.editor;
-    let NoteRef = firebase.database().ref('user-notes/' + currentUser.uid + '/' + note.key);
-    NoteRef.on('value', (snapshot) => {
-      console.log(snapshot.val());
-      let note = Object.assign({}, snapshot.val(), { key: snapshot.ref.key });
-      let content = note.content;
-      this.setState({ note, });
-      // editor.value = content;
-    });
-  };
-
-  handleTitleChange(event) {
-    let { note } = this.state;
-    note.title = event.target.value;
-    this.setState({ note, });
-  }
-
-  handleInputChange(event) {
-    let { note } = this.state;
-    note.content = event.target.value;
-    this.setState({ note, });
-  }
+    handleInputChange(text, meduim) {
+        let { note } = this.state;
+        note.content = text;
+        this.setState({ note, });
+    }
 
 
-  save() {
-    let editor = this.editor;
-    let content = editor.value;
-    let { note } = this.state;
-    note.content = content;
-    this.setState({ note, });
-    Note.save(window.currentUser.uid, note);
-    console.log('saved');
+    save() {
+        let { note } = this.state;
+        Note.save(window.currentUser.uid, note);
+        console.log('saved');
 
-  }
+    }
 
-  render() {
-    return (
-      <div className="editor">
-        <div>
-          <input
-            className="editor-input__title"
-            type="text"
-            value={this.state.note.title}
-            onChange={this.handleTitleChange.bind(this)}/>
-          <textarea
-            className="editor-input__content"
-            value={this.state.note.content}
-            onChange={this.handleInputChange.bind(this)}
-            ref="noteTextarea"
-          />
-        </div>
-        <button className="button button-action button-rounded button-small"
-                onClick={this.save.bind(this)}>保存
-        </button>
-      </div>
-    )
-  }
+    render() {
+        const { editorState } = this.state;
+        return (
+            <div className="editor">
+                <div>
+                    <input
+                        className="editor-input__title"
+                        type="text"
+                        value={this.state.note.title}
+                        onChange={this.handleTitleChange.bind(this)} />
+                    <Editor
+                        ref="editor"
+                        editorState={editorState}
+                        onChange={this.onChange} />
+                </div>
+                <button className="button button-action button-rounded button-small"
+                    onClick={this.save.bind(this)}>保存</button>
+            </div>
+        )
+    }
 }
